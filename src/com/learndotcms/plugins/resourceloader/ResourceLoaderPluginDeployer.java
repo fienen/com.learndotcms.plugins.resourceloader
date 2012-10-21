e987bc67c8aa8078e9697b6621b9f866c26f153b
Michael Fienen <fienen@gmail.com>
2012-10-21 15:59:04 -0500

    Imported initial Web Resource configuration.

--- src/com/learndotcms/plugins/resourceloader/ResourceLoaderPluginDeployer.java
+++ src/com/learndotcms/plugins/resourceloader/ResourceLoaderPluginDeployer.java
@@ -11,66 +11,66 @@ import com.dotmarketing.portlets.structure.factories.StructureFactory;
 public class ResourceLoaderPluginDeployer implements PluginDeployer {
   public boolean deploy() {
     Structure st = new Structure();
-    st.setName("YouTube Video");
-    st.setDescription("Widget for embedding a YouTube video and player in a page.");
+    st.setName("Web Resource");
+    st.setDescription("Common CSS/JS/VTL Files used for sharing and building templates.");
     st.setFixed(false);
-    st.setStructureType(Structure.STRUCTURE_TYPE_WIDGET);
+    st.setStructureType(Structure.STRUCTURE_TYPE_FILEASSET);
     StructureFactory.saveStructure(st);
-    
-    Field field1 = new Field("Video Title", Field.FieldType.TEXT, Field.DataType.TEXT, st, true, true, true, 0, true, false, true);
-    field1.setVelocityVarName("widgetTitle");
-    FieldFactory.saveField(field1);
-
-    Field field2 = new Field("Widget Usage", Field.FieldType.TEXT_AREA, Field.DataType.TEXT, st, false, false, true, 1, true, false, false);
-    field2.setFieldContentlet("constant");
-    FieldFactory.saveField(field2);
-
-    Field field3 = new Field("Widget Code", Field.FieldType.HIDDEN, Field.DataType.TEXT, st, false, false, true, 2, true, false, false);
-    field3.setFieldContentlet("constant");
-    field3.setValues("#set($YTHeight = \"$playerHeight\")\n" +
-    "#set($YTWidth = \"$playerWidth\")\n" +
-    "#set($YTRelated = \"$showRelatedVideos\")\n" +
-    "#set($YTBorder = \"$displayPlayerBorder\")\n" +
-    "#set($YTCookies = \"$enableDelayedCookies\")\n" +
-    "#set($YTColor1 = \"$gradientColor1\")\n" +
-    "#set($YTColor2 = \"$gradientColor2\")\n" +
-    "#set($YTClass = \"$divWrapperClass\")\n" +
-    "#set($YTQuality = \"$videoQuality\")\n" +
-    "#youtube(\"$youtubeVideoId\"");
+	
+	Field field1 = new Field(HOST_FOLDER_FIELD_NAME, Field.FieldType.HOST_OR_FOLDER, Field.DataType.TEXT, st, true, false, true, 1, "", "", "", true, false, true);
+	field1.setVelocityVarName(HOST_FOLDER_FIELD);
+	FieldFactory.saveField(field1);
+	
+	field2 = new Field(BINARY_FIELD_NAME, Field.FieldType.BINARY, Field.DataType.BINARY, st, true, false, false, 2, "", "", "", true, false, false);
+	field2.setVelocityVarName(BINARY_FIELD);
+	FieldFactory.saveField(field2);
+	
+	Field field3 = new Field("External Link (CDN)", Field.FieldType.TEXT, Field.DataType.TEXT, st, false, false, false, 3, "", "", "", false, false, false);
+	field3.setVelocityVarName("cdn");
     FieldFactory.saveField(field3);
+	
+	field4 = new Field(TITLE_FIELD_NAME, Field.FieldType.TEXT, Field.DataType.TEXT, st, true, true, true, 4, "", "", "", true, false, true);
+	field4.setVelocityVarName(TITLE_FIELD);
+	field4.setListed(false);
+	FieldFactory.saveField(field4);
 
-    Field field4 = new Field("YouTube Video ID", Field.FieldType.TEXT, Field.DataType.TEXT, st, true, true, true, 3, true, false, false);
-    FieldFactory.saveField(field4);
-
-    Field field5 = new Field("Optional Parameters", Field.FieldType.LINE_DIVIDER, Field.DataType.SECTION_DIVIDER, st, false, false, false, 4, true, false, false);
-    FieldFactory.saveField(field5);
-    
-    Field field6 = new Field("Video Quality", Field.FieldType.SELECT, Field.DataType.TEXT, st, true, false, false, 5, "Standard|STD\r\nHigh Quality|HQ\r\nHigh Definition|HD", "STD", "", true, false, false);
+	field5 = new Field(FILE_NAME_FIELD_NAME, Field.FieldType.TEXT, Field.DataType.TEXT, st, false, true, true, 5, "", "", "", true, true, true);
+	field5.setVelocityVarName(FILE_NAME_FIELD);
+	FieldFactory.saveField(field5);
+	
+	Field field6 = new Field("Resource ID", Field.FieldType.TEXT, Field.DataType.TEXT, st, false, true, true, 6, "", "", "[0-9a-zA-Z-,]*", false, false, true);
+	field6.setVelocityVarName("resourceId");
     FieldFactory.saveField(field6);
-
-    Field field7 = new Field("Player Height", Field.FieldType.TEXT, Field.DataType.INTEGER, st, true, false, false, 6, "", "344", "[0-9]*", true, false, false);
+	
+	Field field7 = new Field("Default Source", Field.FieldType.RADIO, Field.DataType.TEXT, st, false, true, true, 7, "Local|local\r\nExternal/CDN|cdn", "local", "", false, false, true);
+	field7.setVelocityVarName("source");
     FieldFactory.saveField(field7);
-
-    Field field8 = new Field("Player Width", Field.FieldType.TEXT, Field.DataType.INTEGER, st, true, false, false, 7, "", "425", "[0-9]*", true, false, false);
+	
+	Field field8 = new Field("Additional Code", Field.FieldType.TEXT_AREA, Field.DataType.TEXT, st, false, false, false, 8, "", "", "", false, false, true);
+	field8.setVelocityVarName("code");
     FieldFactory.saveField(field8);
 
-    Field field9 = new Field("Show Related Videos", Field.FieldType.RADIO, Field.DataType.BOOL, st, true, false, false, 8, "Yes|1\r\nNo|0", "1", "", true, false, false);
-    FieldFactory.saveField(field9);
+	field9 = new Field(META_DATA_TAB_NAME, Field.FieldType.TAB_DIVIDER, Field.DataType.SECTION_DIVIDER, st, false, false, false, 9, "", "", "", false, false, false);
+	field9.setVelocityVarName("MetadataTab");
+	FieldFactory.saveField(field9);
 
-    Field field10 = new Field("Display Player Border", Field.FieldType.RADIO, Field.DataType.BOOL, st, true, false, false, 9, "Yes|1\r\nNo|0", "0", "", true, false, false);
-    FieldFactory.saveField(field10);
+	field10 = new Field(META_DATA_FIELD_NAME, Field.FieldType.KEY_VALUE, Field.DataType.LONG_TEXT, st, false, false, false, 10, "", "", "", true, true, true);
+	field10.setVelocityVarName(META_DATA_FIELD);
+	FieldFactory.saveField(field10);
 
-    Field field11 = new Field("Enable Delayed Cookies", Field.FieldType.RADIO, Field.DataType.BOOL, st, true, false, false, 10, "Yes|1\r\nNo|0", "0", "", true, false, false);
-    FieldFactory.saveField(field11);
+	field11 = new Field(SHOW_ON_MENU_NAME, Field.FieldType.CHECKBOX, Field.DataType.TEXT, st, false, false, true, 11, "|true", "false", "", true, false, false);
+	field11.setVelocityVarName(SHOW_ON_MENU);
+	FieldFactory.saveField(field11);
 
-    Field field12 = new Field("Gradient Color 1", Field.FieldType.TEXT, Field.DataType.TEXT, st, false, false, false, 11, "", "", "[a-fA-F0-9]{6}", true, false, false);
-    FieldFactory.saveField(field12);
+	field12 = new Field(SORT_ORDER_NAME, Field.FieldType.TEXT, Field.DataType.INTEGER, st, false, false, true, 12, "", "0", "", true, false, false);
+	field12.setVelocityVarName(SORT_ORDER);
+	FieldFactory.saveField(field12);
 
-    Field field13 = new Field("Gradient Color 2", Field.FieldType.TEXT, Field.DataType.TEXT, st, false, false, false, 12, "", "", "[a-fA-F0-9]{6}", true, false, false);
-    FieldFactory.saveField(field13);
+	field13 = new Field(DESCRIPTION_NAME, Field.FieldType.TEXT, Field.DataType.TEXT, st, false, false, true, 13, "", "", "", true, false, false);
+	field13.setVelocityVarName(DESCRIPTION);
+	FieldFactory.saveField(field13);
 
-    Field field14 = new Field("Div Wrapper Class", Field.FieldType.TEXT, Field.DataType.TEXT, st, true, false, false, 13, "", "youtube_video", "", true, false, false);
-    FieldFactory.saveField(field14);
+	FieldsCache.clearCache();
     
     return false;
   }
