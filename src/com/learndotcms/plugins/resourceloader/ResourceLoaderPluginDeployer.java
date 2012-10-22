b2db089a49e9643b2b14476db467cda25eb1bde7
Michael Fienen <fienen@gmail.com>
2012-10-21 22:10:42 -0500

    Testing deployment process

--- src/com/learndotcms/plugins/resourceloader/ResourceLoaderPluginDeployer.java
+++ src/com/learndotcms/plugins/resourceloader/ResourceLoaderPluginDeployer.java
@@ -1,5 +1,9 @@
 package com.learndotcms.plugins.resourceloader;
 
+import com.dotmarketing.cache.FieldsCache;
+import com.dotmarketing.business.DotStateException;
+import com.dotmarketing.exception.DotDataException;
+import com.dotmarketing.exception.DotRuntimeException;
 import com.dotmarketing.plugin.PluginDeployer;
 import com.dotmarketing.portlets.structure.model.Field;
 import com.dotmarketing.portlets.structure.model.Field.DataType;
@@ -7,75 +11,145 @@ import com.dotmarketing.portlets.structure.model.Field.FieldType;
 import com.dotmarketing.portlets.structure.model.Structure;
 import com.dotmarketing.portlets.structure.factories.FieldFactory;
 import com.dotmarketing.portlets.structure.factories.StructureFactory;
+import com.dotmarketing.util.Logger;
 
 public class ResourceLoaderPluginDeployer implements PluginDeployer {
   public boolean deploy() {
     Structure st = new Structure();
-    st.setName("Web Resource");
+    st.setName("Web Resource Test");
     st.setDescription("Common CSS/JS/VTL Files used for sharing and building templates.");
     st.setFixed(false);
     st.setStructureType(Structure.STRUCTURE_TYPE_FILEASSET);
-    StructureFactory.saveStructure(st);
+	try {
+		StructureFactory.saveStructure(st);
+	} catch (Exception e) {
+		Logger.error(this.getClass(), e.getMessage(), e);
+		throw new DotRuntimeException(e.getMessage(), e);
+	}
 	
-	Field field1 = new Field(HOST_FOLDER_FIELD_NAME, Field.FieldType.HOST_OR_FOLDER, Field.DataType.TEXT, st, true, false, true, 1, "", "", "", true, false, true);
-	field1.setVelocityVarName(HOST_FOLDER_FIELD);
-	FieldFactory.saveField(field1);
+	Field field1 = new Field("Host or Folder", Field.FieldType.HOST_OR_FOLDER, Field.DataType.TEXT, st, true, false, true, 1, "", "", "", true, false, true);
+	field1.setVelocityVarName("hostFolder");
+	try {
+		FieldFactory.saveField(field1);
+	} catch (Exception e) {
+		Logger.error(this.getClass(), e.getMessage(), e);
+		throw new DotRuntimeException(e.getMessage(), e);
+	}
 	
-	field2 = new Field(BINARY_FIELD_NAME, Field.FieldType.BINARY, Field.DataType.BINARY, st, true, false, false, 2, "", "", "", true, false, false);
-	field2.setVelocityVarName(BINARY_FIELD);
-	FieldFactory.saveField(field2);
+	Field field2 = new Field("File Asset", Field.FieldType.BINARY, Field.DataType.BINARY, st, true, false, false, 2, "", "", "", true, false, false);
+	field2.setVelocityVarName("fileAsset");
+	try {
+		FieldFactory.saveField(field2);
+	} catch (Exception e) {
+		Logger.error(this.getClass(), e.getMessage(), e);
+		throw new DotRuntimeException(e.getMessage(), e);
+	}
 	
 	Field field3 = new Field("External Link (CDN)", Field.FieldType.TEXT, Field.DataType.TEXT, st, false, false, false, 3, "", "", "", false, false, false);
 	field3.setVelocityVarName("cdn");
-    FieldFactory.saveField(field3);
+    try {
+		FieldFactory.saveField(field3);
+	} catch (Exception e) {
+		Logger.error(this.getClass(), e.getMessage(), e);
+		throw new DotRuntimeException(e.getMessage(), e);
+	}
 	
-	field4 = new Field(TITLE_FIELD_NAME, Field.FieldType.TEXT, Field.DataType.TEXT, st, true, true, true, 4, "", "", "", true, false, true);
-	field4.setVelocityVarName(TITLE_FIELD);
-	field4.setListed(false);
-	FieldFactory.saveField(field4);
+	Field field4 = new Field("Title", Field.FieldType.TEXT, Field.DataType.TEXT, st, true, false, true, 4, "", "", "", true, false, true);
+	field4.setVelocityVarName("title");
+	try {
+		FieldFactory.saveField(field4);
+	} catch (Exception e) {
+		Logger.error(this.getClass(), e.getMessage(), e);
+		throw new DotRuntimeException(e.getMessage(), e);
+	}
 
-	field5 = new Field(FILE_NAME_FIELD_NAME, Field.FieldType.TEXT, Field.DataType.TEXT, st, false, true, true, 5, "", "", "", true, true, true);
-	field5.setVelocityVarName(FILE_NAME_FIELD);
-	FieldFactory.saveField(field5);
+	Field field5 = new Field("File Name", Field.FieldType.TEXT, Field.DataType.TEXT, st, false, true, true, 5, "", "", "", true, true, true);
+	field5.setVelocityVarName("fileName");
+	try {
+		FieldFactory.saveField(field5);
+	} catch (Exception e) {
+		Logger.error(this.getClass(), e.getMessage(), e);
+		throw new DotRuntimeException(e.getMessage(), e);
+	}
 	
 	Field field6 = new Field("Resource ID", Field.FieldType.TEXT, Field.DataType.TEXT, st, false, true, true, 6, "", "", "[0-9a-zA-Z-,]*", false, false, true);
 	field6.setVelocityVarName("resourceId");
-    FieldFactory.saveField(field6);
+	try {
+		FieldFactory.saveField(field6);
+	} catch (Exception e) {
+		Logger.error(this.getClass(), e.getMessage(), e);
+		throw new DotRuntimeException(e.getMessage(), e);
+	}
 	
 	Field field7 = new Field("Default Source", Field.FieldType.RADIO, Field.DataType.TEXT, st, false, true, true, 7, "Local|local\r\nExternal/CDN|cdn", "local", "", false, false, true);
 	field7.setVelocityVarName("source");
-    FieldFactory.saveField(field7);
+ 	try {
+		FieldFactory.saveField(field7);
+	} catch (Exception e) {
+		Logger.error(this.getClass(), e.getMessage(), e);
+		throw new DotRuntimeException(e.getMessage(), e);
+	}
 	
 	Field field8 = new Field("Additional Code", Field.FieldType.TEXT_AREA, Field.DataType.TEXT, st, false, false, false, 8, "", "", "", false, false, true);
 	field8.setVelocityVarName("code");
-    FieldFactory.saveField(field8);
+    try {
+		FieldFactory.saveField(field8);
+	} catch (Exception e) {
+		Logger.error(this.getClass(), e.getMessage(), e);
+		throw new DotRuntimeException(e.getMessage(), e);
+	}
 
-	field9 = new Field(META_DATA_TAB_NAME, Field.FieldType.TAB_DIVIDER, Field.DataType.SECTION_DIVIDER, st, false, false, false, 9, "", "", "", false, false, false);
+	Field field9 = new Field("Metadata", Field.FieldType.TAB_DIVIDER, Field.DataType.SECTION_DIVIDER, st, false, false, false, 9, "", "", "", false, false, false);
 	field9.setVelocityVarName("MetadataTab");
-	FieldFactory.saveField(field9);
+	try {
+		FieldFactory.saveField(field9);
+	} catch (Exception e) {
+		Logger.error(this.getClass(), e.getMessage(), e);
+		throw new DotRuntimeException(e.getMessage(), e);
+	}
 
-	field10 = new Field(META_DATA_FIELD_NAME, Field.FieldType.KEY_VALUE, Field.DataType.LONG_TEXT, st, false, false, false, 10, "", "", "", true, true, true);
-	field10.setVelocityVarName(META_DATA_FIELD);
-	FieldFactory.saveField(field10);
+	Field field10 = new Field("Metadata", Field.FieldType.KEY_VALUE, Field.DataType.LONG_TEXT, st, false, false, false, 10, "", "", "", true, true, true);
+	field10.setVelocityVarName("metaData");
+	try {
+		FieldFactory.saveField(field10);
+	} catch (Exception e) {
+		Logger.error(this.getClass(), e.getMessage(), e);
+		throw new DotRuntimeException(e.getMessage(), e);
+	}
 
-	field11 = new Field(SHOW_ON_MENU_NAME, Field.FieldType.CHECKBOX, Field.DataType.TEXT, st, false, false, true, 11, "|true", "false", "", true, false, false);
-	field11.setVelocityVarName(SHOW_ON_MENU);
-	FieldFactory.saveField(field11);
+	Field field11 = new Field("Show On Menu", Field.FieldType.CHECKBOX, Field.DataType.TEXT, st, false, false, true, 11, "|true", "false", "", true, false, false);
+	field11.setVelocityVarName("showOnMenu");
+	try {
+		FieldFactory.saveField(field11);
+	} catch (Exception e) {
+		Logger.error(this.getClass(), e.getMessage(), e);
+		throw new DotRuntimeException(e.getMessage(), e);
+	}
 
-	field12 = new Field(SORT_ORDER_NAME, Field.FieldType.TEXT, Field.DataType.INTEGER, st, false, false, true, 12, "", "0", "", true, false, false);
-	field12.setVelocityVarName(SORT_ORDER);
-	FieldFactory.saveField(field12);
+	Field field12 = new Field("Sort Order", Field.FieldType.TEXT, Field.DataType.INTEGER, st, false, false, true, 12, "", "0", "", true, false, false);
+	field12.setVelocityVarName("sortOrder");
+		try {
+		FieldFactory.saveField(field12);
+	} catch (Exception e) {
+		Logger.error(this.getClass(), e.getMessage(), e);
+		throw new DotRuntimeException(e.getMessage(), e);
+	}
 
-	field13 = new Field(DESCRIPTION_NAME, Field.FieldType.TEXT, Field.DataType.TEXT, st, false, false, true, 13, "", "", "", true, false, false);
-	field13.setVelocityVarName(DESCRIPTION);
-	FieldFactory.saveField(field13);
+	Field field13 = new Field("Description", Field.FieldType.TEXT, Field.DataType.TEXT, st, false, false, true, 13, "", "", "", true, false, false);
+	field13.setVelocityVarName("Description");
+		try {
+		FieldFactory.saveField(field13);
+	} catch (Exception e) {
+		Logger.error(this.getClass(), e.getMessage(), e);
+		throw new DotRuntimeException(e.getMessage(), e);
+	}
 
 	FieldsCache.clearCache();
     
-    return false;
+    return true;
   }
 
   public boolean redeploy(String version) {
-    return false;
+    return true;
   }
 }
